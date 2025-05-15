ea6508ebf9b5c7ea4c3f8a88c15aa9e5c75df489
Denis Bochkarev <bochkarevdenis1995@gmail.com>
2025-05-15 23:46:10 +0400

    register categories url in router

--- backend/api/v1/urls.py
+++ backend/api/v1/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-# from .views import CategoryViewSet
+from categories.views import CategoryViewSet
 
 router = DefaultRouter()
-# router.register('categories', CategoryViewSet, basename='categories')
+router.register('categories', CategoryViewSet, basename='category')
 
 urlpatterns = [
     path('', include(router.urls)),
