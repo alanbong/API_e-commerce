9a5ba1c2cfc32f05400abf676a0181e8bebfa96c
Denis Bochkarev <bochkarevdenis1995@gmail.com>
2025-05-15 20:08:26 +0400

    register Category and Subcategory in admin

--- backend/categories/admin.py
+++ backend/categories/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+from .models import Category, SubCategory
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'image_tag')
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ('name', 'slug')
+    ordering = ('name',)
+
+    def image_tag(self, obj):
+        if obj.image:
+            return mark_safe(
+                f'<img src="{obj.image.url}" style="height: 40px;" />'
+            )
+        return '-'
+    image_tag.short_description = 'Превью'
+
+
+@admin.register(SubCategory)
+class SubCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'image_tag')
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ('name', 'slug')
+    ordering = ('name',)
+
+    def image_tag(self, obj):
+        if obj.image:
+            return mark_safe(
+                f'<img src="{obj.image.url}" style="height: 40px;" />'
+            )
+        return '-'
+    image_tag.short_description = 'Превью'
