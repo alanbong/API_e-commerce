f0d6afe2e4471687ce9932a769738e28442cf430
Denis Bochkarev <bochkarevdenis1995@gmail.com>
2025-05-15 23:01:58 +0400

    add REST_FRAMEWORK settings

--- backend/shop_backend/settings.py
+++ backend/shop_backend/settings.py
@@ -126,3 +126,18 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+
+    'DEFAULT_PAGINATION_CLASS': (
+        'rest_framework.pagination.PageNumberPagination',
+    ),
+    'PAGE_SIZE': 10,
+}
