d351cc16ee5b0659622ecbe2e7f7ea38d80c9ff7
Denis Bochkarev <bochkarevdenis1995@gmail.com>
2025-05-16 21:33:05 +0400

    add get_image method and typing

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,15 +1,18 @@
 from rest_framework import serializers
 
-from categories.models import SubCategory
+from typing import List, Dict, Optional
+
 from .models import Product
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    """Сериализатор для модели Category."""
+    """
+    Сериализатор для модели Category.
+    """
     category = serializers.StringRelatedField(read_only=True)
-    subcategory = serializers.PrimaryKeyRelatedField(
-        queryset=SubCategory.objects.all()
-    )
+    subcategory = serializers.StringRelatedField(read_only=True)
+
+    images = serializers.SerializerMethodField()
 
     class Meta:
         model = Product
@@ -19,8 +22,20 @@ class ProductSerializer(serializers.ModelSerializer):
             'slug',
             'description',
             'price',
-            'image',
+            'images',
             'subcategory',
             'category',
         )
         read_only_fields = ('category',)
+
+    def get_images(self, obj) -> List[Dict[str, Optional[str]]]:
+        """
+        Возвращает список изображений с размерами и URL.
+        """
+        sizes = ['small', 'medium', 'large']
+        fields = [obj.image_small, obj.image_medium, obj.image_large]
+
+        return [
+            {'size': size, 'url': image.url if image else None}
+            for size, image in zip(sizes, fields)
+        ]
