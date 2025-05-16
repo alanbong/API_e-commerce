d8babb0a58697807a674a1ce138a77893decb3d6
Denis Bochkarev <bochkarevdenis1995@gmail.com>
2025-05-16 21:33:15 +0400

    add ProductViewSet

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from .models import Product
+from .serializers import ProductSerializer
+
+
+class ProductViewSet(ReadOnlyModelViewSet):
+    """
+    Представление для модели Product.
+    """
+    queryset = Product.objects.select_related('category', 'subcategory').all()
+    serializer_class = ProductSerializer
+    permission_classes = (AllowAny,)
