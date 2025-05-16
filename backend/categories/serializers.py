6694cc59cc4e8572c6c0b747b9d6fd57b94cd719
Denis Bochkarev <bochkarevdenis1995@gmail.com>
2025-05-16 19:44:53 +0400

    add dockstring for subcategory and category serializers

--- backend/categories/serializers.py
+++ backend/categories/serializers.py
@@ -4,13 +4,14 @@ from .models import Category, SubCategory
 
 
 class SubCategorySerializer(serializers.ModelSerializer):
-    """Сериализатор Модели SubCategory."""
+    """Вложенный сериализатор для CategorySerializer."""
     class Meta:
         model = SubCategory
         fields = ('id', 'name', 'slug', 'image')
 
 
 class CategorySerializer(serializers.ModelSerializer):
+    """Сериализатор для модели Category."""
     subcategories = SubCategorySerializer(many=True, read_only=True)
 
     class Meta:
