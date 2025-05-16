5555c254ea80d3c8a537b8604566f88adfac7861
Denis Bochkarev <bochkarevdenis1995@gmail.com>
2025-05-16 23:50:20 +0400

    register products url

--- backend/api/v1/urls.py
+++ backend/api/v1/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+
 from categories.views import CategoryViewSet
+from products.views import ProductViewSet
 
 router = DefaultRouter()
 router.register('categories', CategoryViewSet, basename='category')
+router.register('products', ProductViewSet, basename='products')
 
 urlpatterns = [
     path('', include(router.urls)),
