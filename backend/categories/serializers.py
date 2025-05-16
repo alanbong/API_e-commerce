2dae1d33d5b55e619a95cf10cb280ea2e9665ca3
Denis Bochkarev <bochkarevdenis1995@gmail.com>
2025-05-16 21:32:21 +0400

    add dockstring

--- backend/categories/serializers.py
+++ backend/categories/serializers.py
@@ -4,14 +4,18 @@ from .models import Category, SubCategory
 
 
 class SubCategorySerializer(serializers.ModelSerializer):
-    """Вложенный сериализатор для CategorySerializer."""
+    """
+    Вложенный сериализатор для CategorySerializer.
+    """
     class Meta:
         model = SubCategory
         fields = ('id', 'name', 'slug', 'image')
 
 
 class CategorySerializer(serializers.ModelSerializer):
-    """Сериализатор для модели Category."""
+    """
+    Сериализатор для модели Category.
+    """
     subcategories = SubCategorySerializer(many=True, read_only=True)
 
     class Meta:
