81b184396cbd43fb6ab8de1cfbfd997ca38fe23f
Denis Bochkarev <bochkarevdenis1995@gmail.com>
2025-05-16 23:50:06 +0400

    add Cart and CartItem models

---
+++ backend/cart/models.py
@@ -0,0 +1,78 @@
+from django.contrib.auth import get_user_model
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+
+
+from products.models import Product
+
+User = get_user_model()
+
+
+class Cart(models.Model):
+    """
+    Модель корзины.
+    """
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name='cart',
+        verbose_name='Пользователь',
+    )
+
+    class Meta:
+        ordering = ('user',)
+        verbose_name = 'Корзина'
+
+    def __str__(self):
+        return f'Корзина пользователя {self.user}'
+
+
+class CartItem(models.Model):
+    """
+    Товар в корзине.
+    """
+    cart = models.ForeignKey(
+        Cart,
+        on_delete=models.CASCADE,
+        related_name='items',
+        verbose_name='Корзина'
+    )
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name='cart_item',
+        verbose_name='Товар в корзине'
+    )
+    quantity = models.PositiveIntegerField(
+        default=1,
+        verbose_name='Количество товаров',
+        validators=[
+            MinValueValidator(
+                1, message=(
+                    'Минимальное количество товаров - 1'
+                )
+            ),
+            MaxValueValidator(
+                9999, message=(
+                    'Максимальное количество товаров - 9999'
+                )
+            )
+        ]
+    )
+
+    class Meta:
+        ordering = ('product',)
+        verbose_name = 'Элемент корзины'
+        verbose_name_plural = 'Элементы корзины'
+        constraints = [
+            models.UniqueConstraint(
+                fields=('cart', 'product'),
+                name='unique_cart_product'
+            )
+        ]
+
+    def __str__(self):
+        return (
+            f'{self.product.name} {self.quantity}шт. '
+            f'в корзине {self.cart.user}'
+        )
