58d6c64ad71c067413ce231d50e344fdd2212604
Denis Bochkarev <bochkarevdenis1995@gmail.com>
2025-05-16 19:45:05 +0400

    add constants for imagekit lib

--- backend/products/constants.py
+++ backend/products/constants.py
@@ -5,3 +5,13 @@ PRODUCT_NAME_MAX_LENGTH = 256
 PRODUCT_SLUG_MAX_LENGTH = 256
 # Максимальная длина для метода __str__ у моделей
 STR_REPR_MAX_LENGTH = 50
+# Размеры изображений
+IMAGE_SMALL_SIZE = (100, 100)
+IMAGE_MEDIUM_SIZE = (300, 300)
+IMAGE_LARGE_SIZE = (600, 600)
+# Качество изображений
+IMAGE_SMALL_QUALITY = 60
+IMAGE_MEDIUM_QUALITY = 75
+IMAGE_LARGE_QUALITY = 90
+# Формат
+IMAGE_FORMAT = 'JPEG'
