7ef0627995f94c37a1f3cb8fdddcba235991531b
Denis Bochkarev <bochkarevdenis1995@gmail.com>
2025-05-16 19:45:43 +0400

    add constants for imagekit, imagekit virtual fields

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from imagekit.models import ImageSpecField
+from imagekit.processors import ResizeToFill
 
 from categories.models import Category, SubCategory
 from .constants import (
-    PRODUCT_NAME_MAX_LENGTH,
-    PRODUCT_SLUG_MAX_LENGTH,
+    PRODUCT_NAME_MAX_LENGTH, PRODUCT_SLUG_MAX_LENGTH,
     STR_REPR_MAX_LENGTH,
+    IMAGE_SMALL_SIZE, IMAGE_MEDIUM_SIZE, IMAGE_LARGE_SIZE,
+    IMAGE_SMALL_QUALITY, IMAGE_MEDIUM_QUALITY, IMAGE_LARGE_QUALITY,
+    IMAGE_FORMAT
 )
 
 
@@ -31,23 +35,12 @@ class Product(models.Model):
         decimal_places=2,
         verbose_name='Цена продукта в рублях',
     )
-    image_large = models.ImageField(
-        upload_to='products/large/',
-        verbose_name='Изображение (большое)',
-    )
-    image_medium = models.ImageField(
-        upload_to='products/medium/',
-        verbose_name='Изображение (среднее)',
-    )
-    image_small = models.ImageField(
-        upload_to='products/small/',
-        verbose_name='Изображение (маленькое)',
-    )
     category = models.ForeignKey(
         Category,
         on_delete=models.CASCADE,
         related_name='products',
         verbose_name='Категория',
+        editable=False,
     )
     subcategory = models.ForeignKey(
         SubCategory,
@@ -55,11 +48,46 @@ class Product(models.Model):
         related_name='products',
         verbose_name='Подкатегория'
     )
+    # Поле с оригинальным изображением
+    image = models.ImageField(
+        upload_to='products/',
+        verbose_name='Изображение продукта',
+    )
+    # Виртуальные (не сохраняются в БД) изображения разных размеров.
+    # Генерируются на основе оригинального изображения при первом обращении.
+    # Хранятся в /CACHE/images.
+    image_small = ImageSpecField(
+        source='image',
+        processors=[ResizeToFill(*IMAGE_SMALL_SIZE)],
+        format=IMAGE_FORMAT,
+        options={'quality': IMAGE_SMALL_QUALITY}
+    )
+    image_medium = ImageSpecField(
+        source='image',
+        processors=[ResizeToFill(*IMAGE_MEDIUM_SIZE)],
+        format=IMAGE_FORMAT,
+        options={'quality': IMAGE_MEDIUM_QUALITY}
+    )
+    image_large = ImageSpecField(
+        source='image',
+        processors=[ResizeToFill(*IMAGE_LARGE_SIZE)],
+        format=IMAGE_FORMAT,
+        options={'quality': IMAGE_LARGE_QUALITY}
+    )
 
     class Meta:
         ordering = ('category', 'subcategory', 'name',)
         verbose_name = 'Продукт'
         verbose_name_plural = 'Продукты'
+
+    def save(self, *args, **kwargs):
+        '''
+        Переопределяем метод save, чтобы автоматически
+        устанавливать категорию на основе выбранной подкатегории subcategory.
+        '''
+        if self.subcategory:
+            self.category = self.subcategory.category
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name[:STR_REPR_MAX_LENGTH]
