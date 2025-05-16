62633afc704afc05ee4834f32cb97889197c4b59
Denis Bochkarev <bochkarevdenis1995@gmail.com>
2025-05-16 17:33:27 +0400

    commenting bug

--- backend/shop_backend/settings.py
+++ backend/shop_backend/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'versatileimagefield',
 
     'categories.apps.CategoriesConfig',
-    'products.apps.ProductsConfig'
+    # 'products.apps.ProductsConfig'
 ]
 
 MIDDLEWARE = [
