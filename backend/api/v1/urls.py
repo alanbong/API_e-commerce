6333893e7859453d7778b380ceffcb9b8c17f0e8
Denis Bochkarev <bochkarevdenis1995@gmail.com>
2025-05-21 02:26:34 +0400

    add shema url

--- backend/api/v1/urls.py
+++ backend/api/v1/urls.py
@@ -1,17 +1,35 @@
 from django.urls import path, include
+from django.views.static import serve
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken.views import obtain_auth_token
+
+from pathlib import Path
+from shop_backend.settings import BASE_DIR
 
 from categories.views import CategoryViewSet
 from products.views import ProductViewSet
 from cart.views import CartViewSet
+
+# DOCS_DIR = Path(__file__).resolve().parent.parent.parent / 'utils'
+DOCS_DIR = BASE_DIR / 'utils'
 
 router = DefaultRouter()
 router.register('categories', CategoryViewSet, basename='category')
 router.register('products', ProductViewSet, basename='products')
 router.register(r'cart', CartViewSet, basename='cart')
 
+
 urlpatterns = [
     path('', include(router.urls)),
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+    path(
+        'redoc/', serve,
+        {'path': 'redoc.html', 'document_root': DOCS_DIR},
+        name='redoc'
+    ),
+    path(
+        'redoc/openapi-schema.yml/', serve,
+        {'path': 'openapi-schema.yml', 'document_root': DOCS_DIR},
+        name='schema-yaml'
+    ),
 ]
